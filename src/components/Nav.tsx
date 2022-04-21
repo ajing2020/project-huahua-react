3c3fac774c42eff26bf9df2fa92c47583328d73c
ajing2020 <1277483876@qq.com>
2022-04-21 15:42:03 +0800

    项目构建 引用styled-component 以及导航栏 基本框架制作

---
+++ src/components/Nav.tsx
@@ -0,0 +1,56 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import React from 'react'
+import Icon from './Icon'
+
+const NavWrapper = styled.nav`
+  line-height: 24px;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  > ul {
+    display: flex;
+    > li {
+      text-align: center;
+      width: 33.3333%;
+      > a {
+        display: flex;
+        padding: 4px 0;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        .icon {
+          width: 24px;
+          height: 24px;
+        }
+      }
+    }
+  }
+`
+
+const Nav = () => {
+  return (
+    <NavWrapper>
+      <ul>
+        <li>
+          <Link to="/tags">
+            <Icon name="tag" />
+            标签页
+          </Link>
+        </li>
+        <li>
+          <Link to="/money">
+            <Icon name="money" />
+            记账页
+          </Link>
+        </li>
+        <li>
+          <Link to="/statistics">
+            <Icon name="chart" />
+            统计页
+          </Link>
+        </li>
+      </ul>
+    </NavWrapper>
+  )
+}
+
+export default Nav
---
+++ src/views/NoMatch.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function NoMatch() {
+  return <div>页面不存在，404</div>
+}
+
+export default NoMatch
