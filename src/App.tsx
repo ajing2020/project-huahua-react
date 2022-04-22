32ae298dbac564d9c0dbeb1625b6d780e70e7fe0
ajing2020 <1277483876@qq.com>
2022-04-22 16:14:24 +0800

    tag标签跳转带参数基本搞定

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import Money from './views/Money'
 import Statistics from './views/Statistics'
 import Tags from './views/Tags'
-import EditTag from './views/EditTag'
+import { EditTag } from './views/EditTag'
 import NoMatch from './views/NoMatch'
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/tags/:tag" exact>
+        <Route path="/tags/:id" exact>
           <EditTag />
         </Route>
         <Route path="/tags" exact>
--- src/views/EditTag.tsx
+++ src/views/EditTag.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import { useTags } from '../useTags'
+import { useParams } from 'react-router-dom'
 
-export default function EditTag() {
-  return <div>EditTag</div>
+type Params = {
+  id: string
 }
+const EditTag: React.FC = (props) => {
+  const { findTag } = useTags()
+  let { id } = useParams<Params>()
+  const tag = findTag(parseInt(id))
+  return <div>{tag.name}</div>
+}
+
+export { EditTag }
